569a6c3b27a021a69a031a3788154be82910078c
Nick Wong <njwon4@gmail.com>
2016-04-05 10:53:09 +1000

    FIT3042 Assignment 1 | initial skeleton for rledecode.c

---
+++ FIT3042_Assignment/Assignment1/rledecode.c
@@ -0,0 +1,28 @@
+
+void arg_parser() {
+
+}
+
+void convert_to_ppm() {
+
+}
+
+void output_to_files() {
+
+}
+
+void output_to_stdout() {
+
+}
+
+void adjust_scale() {
+
+}
+
+void apply_tween() {
+	
+}
+
+int main() {
+
+}
